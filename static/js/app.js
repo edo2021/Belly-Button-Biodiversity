1870dc030cb08280b020f2b0792deb1c60aad914
edris gemtessa <edogemtessa@gmail.com>
2021-04-25 14:25:56 -0500

    function to draw function

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,25 @@
 //starting codes comes from insrtuctor Dom 
 console.log("app.js loaded");
-
+//function to draw graph
 function DrawBargraph(sampleId) {
     console.log(`DrawBargraph(${sampleId})`);
 
     d3.json("data/samples.json").then(data => {
         console.log(data);
 
-        
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
