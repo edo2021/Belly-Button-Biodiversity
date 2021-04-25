12b6bf760bb249f4159def363b1618dd92019b99
edris gemtessa <edogemtessa@gmail.com>
2021-04-25 14:38:14 -0500

    draw graphs

--- static/js/app.js
+++ static/js/app.js
@@ -156,3 +156,18 @@ function initDashboard() {
     var selector = d3.select("#selDataset");
 
     
+    d3.json("data/samples.json").then(data => {
+        console.log(data);
+
+        var sampleNames = data.names;
+
+        sampleNames.forEach(sampleId => {
+            selector.append("option")
+            .text(sampleId)
+            .property("value", sampleId);
+
+        });
+
+        var id = sampleNames[0];
+
+        
